d5ec13aaf3c83ba764365b6553910c768ffed84b
Vlad Poskatcheev <randomvlad@gmail.com>
2026-01-09 19:03:10 -0800

    fix(UnitTest): Fix Unit Test

--- src/test/java/tictactoe/game/GameServiceTest.java
+++ src/test/java/tictactoe/game/GameServiceTest.java
@@ -2,20 +2,20 @@ package tictactoe.game;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.only;
 import static org.mockito.Mockito.verify;
 
-import java.util.Arrays;
 import java.util.List;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import tictactoe.game.dao.GameRepository;
 import tictactoe.game.dao.model.Game;
 import tictactoe.game.dao.model.Game.GameState;
 import tictactoe.game.dao.model.Game.PlayerNumber;
 import tictactoe.game.dao.model.Game.PlayerType;
-import tictactoe.game.dao.GameRepository;
 import tictactoe.user.dao.model.AppUser;
 
 @ExtendWith(MockitoExtension.class)
@@ -29,11 +29,15 @@ class GameServiceTest {
 
     @Test
     void create_PlayerGoFirst_Player1TypeHuman() {
+        // given
         boolean playerGoFirst = true;
         AppUser appUser = new AppUser();
 
+        // when
         Game game = service.create(appUser, playerGoFirst);
 
+
+        // then
         assertThat(game.getAppUser()).isSameAs(appUser);
         assertThat(game.getState()).isEqualTo(GameState.IN_PROGRESS);
         assertThat(game.getNextMove()).isEqualTo(PlayerNumber.PLAYER_1);
@@ -47,11 +51,14 @@ class GameServiceTest {
 
     @Test
     void create_PlayerGoSecond_Player2TypeHuman() {
+        // given
         boolean playerGoFirst = false;
         AppUser appUser = new AppUser();
 
+        // when
         Game game = service.create(appUser, playerGoFirst);
 
+        // then
         assertThat(game.getAppUser()).isSameAs(appUser);
         assertThat(game.getState()).isEqualTo(GameState.IN_PROGRESS);
         assertThat(game.getNextMove()).isEqualTo(PlayerNumber.PLAYER_1);
@@ -65,17 +72,25 @@ class GameServiceTest {
 
     @Test
     void getLastGame_Call_DelegateToRepository() {
+        // given
         AppUser appUser = new AppUser();
+
+        // when
         service.getLastGame(appUser);
-        verify(mockRepository).findFirstByAppUserOrderByIdDesc(appUser);
+
+        // then
+        verify(mockRepository, only()).findFirstByAppUserOrderByIdDesc(appUser);
     }
 
     @Test
     void takeTurn_NewGameTwoMoves_GameInProgress() {
+        // given
         Game game = service.create(new AppUser(), true);
 
+        // when
         service.takeTurn(game, "1-1");
 
+        // then
         assertThat(game.getNextMove()).isEqualTo(PlayerNumber.PLAYER_2);
         assertThat(game.getState()).isEqualTo(GameState.IN_PROGRESS);
         assertRows(//@formatter:off
@@ -85,8 +100,10 @@ class GameServiceTest {
                 "", "", ""
         );//@formatter:on
 
+        // when
         service.takeTurn(game, "0-1");
 
+        // then
         assertThat(game.getNextMove()).isEqualTo(PlayerNumber.PLAYER_1);
         assertThat(game.getState()).isEqualTo(GameState.IN_PROGRESS);
         assertRows(//@formatter:off
@@ -99,11 +116,14 @@ class GameServiceTest {
 
     @Test
     void takeTurn_Player1Wins_GameOverPlayer1Win() {
+        // given
         Game game = service.create(new AppUser(), true);
-        game.getRows().set(0, List.of("x", "x", ""));
+        setRow(game, 0, "x", "x", "");
 
+        // when
         service.takeTurn(game, "0-2");
 
+        // then
         assertThat(game.getNextMove()).isNull();
         assertThat(game.getState()).isEqualTo(GameState.PLAYER_1_WIN);
         assertRows(//@formatter:off
@@ -116,27 +136,33 @@ class GameServiceTest {
 
     @Test
     void takeTurn_Player2Wins_GameOverPlayer2Win() {
+        // given
         Game game = service.create(new AppUser(), true);
-        game.getRows().set(0, List.of("x", "x", "o"));
-        game.getRows().set(1, List.of("", "o", ""));
-        game.getRows().set(2, List.of("", "", ""));
+        setRow(game, 0, "x", "x", "o");
+        setRow(game, 1, "", "o", "");
+        setRow(game, 2, "", "", "");
 
+        // when
         service.takeTurn(game, "1-0"); // player 1 marks X
         service.takeTurn(game, "2-0"); // player 2 marks O and wins
 
+        // then
         assertThat(game.getNextMove()).isNull();
         assertThat(game.getState()).isEqualTo(GameState.PLAYER_2_WIN);
     }
 
     @Test
     void takeTurn_NoWinners_GameOverDraw() {
+        // given
         Game game = service.create(new AppUser(), true);
-        game.getRows().set(0, List.of("o", "x", "x"));
-        game.getRows().set(1, List.of("x", "o", "o"));
-        game.getRows().set(2, List.of("o", "", "x"));
+        setRow(game, 0, "o", "x", "x");
+        setRow(game, 1, "x", "o", "o");
+        setRow(game, 2, "o", "", "x");
 
+        // when
         service.takeTurn(game, "2-1");
 
+        // then
         assertThat(game.getNextMove()).isNull();
         assertThat(game.getState()).isEqualTo(GameState.DRAW);
     }
@@ -149,5 +175,12 @@ class GameServiceTest {
         assertThat(rows.get(0)).containsExactly(tiles[0], tiles[1], tiles[2]);
         assertThat(rows.get(1)).containsExactly(tiles[3], tiles[4], tiles[5]);
         assertThat(rows.get(2)).containsExactly(tiles[6], tiles[7], tiles[8]);
+    }
+
+    private void setRow(Game game, int rowIndex, String value0, String value1, String value2) {
+        List<String> row = game.getRows().get(rowIndex);
+        row.set(0, value0);
+        row.set(1, value1);
+        row.set(2, value2);
     }
 }
